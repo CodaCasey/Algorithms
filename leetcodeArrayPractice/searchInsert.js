6e8247df89d3e026ce8b322a68c53032f8d7300e
Casey Jackson <ctjacks3@gmail.com>
2020-10-23 11:48:44 -0700

    added some toy problems

---
+++ leetcodeArrayPractice/searchInsert.js
@@ -0,0 +1,18 @@
+/*
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+*/
+
+var searchInsert = function(nums, target) {
+  let test = 0;
+  for(let i = 0; i<nums.length; i++){
+      if(nums[i] === target){
+          return i
+      }else{
+          if(target > nums[i]){
+              test ++
+          }
+      }
+  }
+  return test
+};
+
